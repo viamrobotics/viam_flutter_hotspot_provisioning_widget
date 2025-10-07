eddb6b8e5e4e76158585953e870c9240b3c0de87
Julie Krasnick <julie.krasnick@viam.com>
2025-10-07 16:04:25 -0400

    change name from robot to machine

--- lib/src/screens/confirmation.dart/view_model/confirmation_view_model.dart
+++ lib/src/screens/confirmation.dart/view_model/confirmation_view_model.dart
@@ -4,7 +4,7 @@ class ConfirmationViewModel extends ChangeNotifier {
   ConfirmationViewModel({
     required Viam viam,
     required Robot robot,
-    required void Function(Robot robot, RobotStatus status) onStatusDetermined,
+    required void Function(Robot robot, MachineStatus status) onStatusDetermined,
     required String? fragmentId,
     required RobotPart mainPart,
     required bool overrideFragment,
@@ -26,26 +26,26 @@ class ConfirmationViewModel extends ChangeNotifier {
   final Robot _robot;
   final String? _fragmentId;
   final RobotPart _mainPart;
-  final void Function(Robot robot, RobotStatus status) _onStatusDetermined;
+  final void Function(Robot robot, MachineStatus status) _onStatusDetermined;
   final bool _overrideFragment;
   final bool _replaceHardware;
   final Map<String, dynamic>? _robotConfig;
 
   Timer? _timer;
-  RobotStatus _robotStatus = RobotStatus.loading;
+  MachineStatus _machineStatus = MachineStatus.loading;
   int _secondsLoading = 0;
 
   static const int provisioningTimeoutSeconds = 120;
   static const int provisioningStillWaitingSeconds = 45;
 
-  RobotStatus get robotStatus => _robotStatus;
+  MachineStatus get machineStatus => _machineStatus;
   int get secondsLoading => _secondsLoading;
 
-  void _setRobotStatus(RobotStatus value) {
-    if (_robotStatus != value) {
-      _robotStatus = value;
+  void _setMachineStatus(MachineStatus value) {
+    if (_machineStatus != value) {
+      _machineStatus = value;
       notifyListeners();
-      _whenFinalRobotStatusIsDetermined();
+      _whenFinalMachineStatusIsDetermined();
     }
   }
 
@@ -60,10 +60,10 @@ class ConfirmationViewModel extends ChangeNotifier {
 
 // triggers a callback to the hotspot provisioning flow to to indicate if the robot is online or offline
 // Also, at this point we know we are online so we can call the fragment override.
-  void _whenFinalRobotStatusIsDetermined() {
+  void _whenFinalMachineStatusIsDetermined() {
     // robot provisioned succesfully and is online
-    if (_robotStatus == RobotStatus.online) {
-      _onStatusDetermined.call(_robot, RobotStatus.online);
+    if (_machineStatus == MachineStatus.online) {
+      _onStatusDetermined.call(_robot, MachineStatus.online);
       _timer?.cancel();
       if (_overrideFragment) {
         _performFragmentOverride(_viam, _fragmentId, _mainPart, _robot);
@@ -72,16 +72,16 @@ class ConfirmationViewModel extends ChangeNotifier {
         _applyRobotConfig(_viam, _mainPart, _robotConfig);
       }
       // robot provisioning did not complete successfully and is offline
-    } else if (_robotStatus == RobotStatus.offline) {
-      _onStatusDetermined.call(_robot, RobotStatus.offline);
+    } else if (_machineStatus == MachineStatus.offline) {
+      _onStatusDetermined.call(_robot, MachineStatus.offline);
       _timer?.cancel();
     }
   }
 
   void _checkForTimeout() {
-    if (_robotStatus == RobotStatus.loading && _secondsLoading >= provisioningTimeoutSeconds) {
+    if (_machineStatus == MachineStatus.loading && _secondsLoading >= provisioningTimeoutSeconds) {
       debugPrint('robot provisioning timed out and is offline');
-      _setRobotStatus(RobotStatus.offline);
+      _setMachineStatus(MachineStatus.offline);
     }
   }
 
@@ -89,7 +89,7 @@ class ConfirmationViewModel extends ChangeNotifier {
     if (_timer != null && _timer!.isActive) return;
 
     _timer = Timer.periodic(const Duration(seconds: 5), (timer) {
-      _getRobotStatus();
+      _getMachineStatus();
       _setSecondsLoading(_secondsLoading + 5);
     });
   }
@@ -119,28 +119,28 @@ class ConfirmationViewModel extends ChangeNotifier {
     }
   }
 
-  void _getRobotStatus() async {
+  void _getMachineStatus() async {
     try {
       final reloadedRobot = await _viam.appClient.getRobot(_robot.id);
-      final newRobotStatus = await calculateRobotStatus(reloadedRobot);
-      debugPrint('Robot status: $newRobotStatus, name: ${reloadedRobot.name}');
-      if (newRobotStatus == RobotStatus.online) {
+      final newMachineStatus = await calculateMachineStatus(reloadedRobot);
+      debugPrint('Robot status: $newMachineStatus, name: ${reloadedRobot.name}');
+      if (newMachineStatus == MachineStatus.online) {
         _timer?.cancel();
       }
-      _setRobotStatus(newRobotStatus);
+      _setMachineStatus(newMachineStatus);
     } catch (e) {
       // if an error, that means we still lack network connection
       debugPrint('Error getting robot status ${e.toString()}');
     }
   }
 
-  Future<RobotStatus> calculateRobotStatus(Robot robot) async {
+  Future<MachineStatus> calculateMachineStatus(Robot robot) async {
     final seconds = robot.lastAccess.seconds.toInt();
     final actual = DateTime.now().microsecondsSinceEpoch / Duration.microsecondsPerSecond;
     if ((actual - seconds) < 10) {
-      return RobotStatus.online;
+      return MachineStatus.online;
     }
-    return RobotStatus.loading;
+    return MachineStatus.loading;
   }
 
   @override
--- lib/src/utils/hotspot_provisioning_result.dart
+++ lib/src/utils/hotspot_provisioning_result.dart
@@ -2,7 +2,7 @@ part of '../../viam_flutter_hotspot_provisioning_widget.dart';
 
 class HotspotProvisioningResult {
   final Robot robot;
-  final RobotStatus status;
+  final MachineStatus status;
 
   HotspotProvisioningResult({required this.robot, required this.status});
 }
