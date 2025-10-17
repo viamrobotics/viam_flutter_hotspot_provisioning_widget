e2a4f24df5e7275913d560ce0069b993cac38894
Julie Krasnick <99275379+jckras@users.noreply.github.com>
2025-10-17 16:14:27 -0400

    Pass in viewModel to PasswordInputScreen (#66)
    * unused viam
    
    * initialize viewModels consistently
    
    * small fix

--- lib/src/flow/hotspot_provisioning_flow.dart
+++ lib/src/flow/hotspot_provisioning_flow.dart
@@ -138,7 +138,6 @@ class _HotspotProvisioningFlowState extends State<HotspotProvisioningFlow> {
             NetworkSelectionScreen(
               onBack: _goToPreviousPage,
               viewModel: viewModel.networkSelectionViewModel,
-              viam: widget.viam,
               onSelectNetwork: (network) {
                 viewModel.onNetworkSelected(network);
               },
@@ -146,7 +145,7 @@ class _HotspotProvisioningFlowState extends State<HotspotProvisioningFlow> {
                 viewModel.onNetworkSelected(null);
               },
             ),
-            PasswordInputScreen(onBack: _goToPreviousPage),
+            PasswordInputScreen(viewModel: viewModel.passwordInputViewModel, onBack: _goToPreviousPage),
             ConfirmationScreen(
               robot: widget.robot,
               viam: widget.viam,
