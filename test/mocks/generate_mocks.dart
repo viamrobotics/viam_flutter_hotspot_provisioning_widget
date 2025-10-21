e1e30b8b8657e6d26648bbdd55d9defad55448a6
Julie Krasnick <julie.krasnick@viam.com>
2025-10-21 14:24:14 -0400

    add tests

---
+++ test/mocks/generate_mocks.dart
@@ -0,0 +1,11 @@
+import 'package:mockito/annotations.dart';
+import 'package:viam_flutter_hotspot_provisioning_widget/viam_flutter_hotspot_provisioning_widget.dart';
+import 'package:viam_sdk/src/app/app.dart';
+
+@GenerateMocks([
+  HotspotProvisioningRepository,
+  Viam,
+  ProvisioningClient,
+  AppClient,
+])
+void main() {}
