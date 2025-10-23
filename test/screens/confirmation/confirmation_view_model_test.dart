995dd4aeaad2aa4e1b88e439e27d920ebd10cb73
Julie Krasnick <julie.krasnick@viam.com>
2025-10-23 11:47:40 -0400

    make public

---
+++ test/screens/confirmation/confirmation_view_model_test.dart
@@ -0,0 +1,39 @@
+import '../../mocks/generate_mocks.mocks.dart';
+
+void main() {
+  
+
+  setUp(() {
+  
+  });
+
+  group('startCheckingOnline', () {
+    test('', () {
+     
+    });
+  });
+
+   group('disconnectFromHotspot', () {
+    test('', () {});
+  });
+
+   group('performFragmentOverride', () {
+    test('', () {});
+  });
+
+   group('applyRobotConfig', () {
+    test('', () {});
+  });
+
+   group('dispose', () {
+    test('', () {});
+  });
+
+   group('_addMachineStatus', () {
+    test('', () {});
+  });
+
+   group('_closeMachineStatusStream', () {
+    test('', () {});
+  });
+}
