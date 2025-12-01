3316b436cd9b2f3eefb8600e3761decf4d7bb043
Julie Krasnick <julie.krasnick@viam.com>
2025-12-01 14:06:57 -0500

    mock resetSubmitting

--- test/flow/hotspot_provisioning_flow_view_model_test.dart
+++ test/flow/hotspot_provisioning_flow_view_model_test.dart
@@ -58,7 +58,9 @@ void main() {
   });
   group('test onCredentialsSubmitted', () {
     test('should prioritize user credentials over configured ones when user credentials are submitted by the user', () async {
+      when(mockHotspotCredentialsInputViewModel.resetSubmitting()).thenAnswer((_) async {});
       hotspotProvisioningFlowViewModel.onCredentialsSubmitted('user-prefix', 'user-password');
+
 
       expect(hotspotProvisioningFlowViewModel.hotspotPrefix, equals('user-prefix'));
       expect(hotspotProvisioningFlowViewModel.hotspotPassword, equals('user-password'));
@@ -69,6 +71,7 @@ void main() {
       expect(hotspotProvisioningFlowViewModel.hotspotPassword, equals('test-password'));
     });
     test('should reset isSubmitting after onCredentialsSubmitted is called', () {
+      when(mockHotspotCredentialsInputViewModel.resetSubmitting()).thenAnswer((_) async {});
       hotspotProvisioningFlowViewModel.onCredentialsSubmitted('user-prefix', 'user-password');
 
       verify(mockHotspotCredentialsInputViewModel.resetSubmitting()).called(1);
